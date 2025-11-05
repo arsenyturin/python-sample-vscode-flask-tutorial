727810079c568b962e98da458b9cf15640d15031
Arseny Turin <40893551+arsenyturin@users.noreply.github.com>
2025-11-04 19:42:32 -0500

    Update startup.py

--- startup.py
+++ startup.py
@@ -8,5 +8,5 @@ The solution is to provide a simple alternate startup file, like this present
 startup.py, that just imports the app object. You can then just specify
 startup:app in the Gunicorn command.
 """
-
+# Adding comment to trigger Pipeline
 from hello_app.webapp import app
